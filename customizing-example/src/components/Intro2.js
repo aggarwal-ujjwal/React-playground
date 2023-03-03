a2aca58466c0b1208ae5a9087e76949521119454
Ujjwal Aggarwal <aggarwal.ujjwal.17@gmail.com>
2023-03-03 23:05:54 +0530

    Correcting intro2 file code

--- customizing-example/src/components/Intro2.js
+++ customizing-example/src/components/Intro2.js
@@ -1,13 +1,13 @@
-function Intro1() {
-        return (
-            <div className="blog-post-intro">
-                <h2>I've become a React developer!</h2>
-                <div>
-                    <p>I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
-                    <p className="link">Read more...</p>
-                </div>
+function Intro2() {
+    return (
+        <div className="blog-post-intro">
+            <h2>Why I love front-end web development</h2>
+            <div>
+                <p>In this blog post, I'll list 10 reasons why I love to work as a front-end developer.</p>
+                <p className="link">Read more...</p>
             </div>
-        );
-    };
-    
-    export default Intro1;
+        </div>
+    );
+};
+
+export default Intro2;
