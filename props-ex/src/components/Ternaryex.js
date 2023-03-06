7b3086dce92a1cec997f57b182c93f956008133c
Ujjwal Aggarwal <aggarwal.ujjwal.17@gmail.com>
2023-03-06 21:43:50 +0530

    refactor ternary

--- props-ex/src/components/Ternaryex.js
+++ props-ex/src/components/Ternaryex.js
@@ -1,8 +1,10 @@
 function Ternaryex(){
+    let num = Math.random();
     return (
-        <div className='App-header'>
+        <div className='App-main'>
             <h1>
-                {Math.random() >0.5 ? "Greater than 0.5" : "Less than equal to 0.5"}
+
+                Random number {num} - { num >0.5 ? "Greater than 0.5" : "Less than equal to 0.5"}
             </h1>
         </div>
         
