efd7baabcb448e4e75e696aa2996022553909723
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-03-23 17:37:24 +0530

    MouseOver example

--- Week 2/event-example/src/Button.js
+++ Week 2/event-example/src/Button.js
@@ -1,9 +1,21 @@
+// function Button(){
+//     const clickHandler = 
+//         () => console.log('Clicked')
+//     return (
+//         <button onClick={clickHandler}>
+//             Click me
+//             </button>
+//     );
+// }
+
+// export default Button;
+
 function Button(){
     const clickHandler = 
-        () => console.log('Clicked')
+        () => console.log('Mouse Over')
     return (
-        <button onClick={clickHandler}>
-            Click me
+        <button onMouseOver={clickHandler}>
+            Mouse Over
             </button>
     );
 }
